29c3c80531d5ff3a90b818347aabc460d046693d
Felix Schreiber <felixschreiber89@gmail.com>
2025-01-04 15:08:49 +0100

    Finished Day4 PartTwo...still needs refactoring

--- src/Days/Day4/PartTwo/ExtractorAnalyzer.java
+++ src/Days/Day4/PartTwo/ExtractorAnalyzer.java
@@ -1,6 +1,7 @@
 package Days.Day4.PartTwo;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,9 @@ public class ExtractorAnalyzer {
 
     private final IndexExtractor extractor;
     private final Map<Integer[], Integer> analyzedFunctionalPhrases;
-    private final char[] charsToLookFor = new char[]{'X', 'M', 'A', 'S'};
+    private final Collection<Integer> values;
+    private final char[] charsToLookFor = new char[]{'A', 'M', 'S'};
+    private final Map<Integer, Integer[]> coordinatesToCheck;
     private final int firstX = 0;
     private final int firstY = 0;
     private final int lastX;
@@ -17,13 +20,26 @@ public class ExtractorAnalyzer {
 
     public ExtractorAnalyzer() throws IOException {
         this.extractor = new IndexExtractor();
-        this.lastX = extractor.getLengthX();
-        this.lastY = extractor.getLengthY();
+        this.lastX = extractor.getLengthX() - 1;
+        this.lastY = extractor.getLengthY() - 1;
+        this.coordinatesToCheck = getCoordinates();
         this.analyzedFunctionalPhrases = analyzeExtractedData();
+        this.values = analyzedFunctionalPhrases.values();
+    }
+
+    private Map<Integer, Integer[]> getCoordinates() {
+        Map<Integer, Integer[]> map = new HashMap<>();
+        map.put(1, new Integer[]{2, 0});
+        map.put(2, new Integer[]{0, -2});
+        map.put(3, new Integer[]{2, -2});
+        map.put(4, new Integer[]{-2, 0});
+        map.put(5, new Integer[]{0, 2});
+        map.put(6, new Integer[]{-2, 2});
+        return map;
     }
 
     private Map<Integer[], Integer> analyzeExtractedData() {
-        List<Integer[]> data = extractor.getExpressions().getFirst();
+        List<Integer[]> data = extractor.getExpressions().get(2);
         Map<Integer[], Integer> map = new HashMap<>();
 
         for (int i = 0; i < data.size(); i++) {
@@ -33,44 +49,65 @@ public class ExtractorAnalyzer {
     }
 
     private Integer validatePhrase(Integer yIndex, Integer xIndex) {
-        Integer validWords = 0;
-        for (int i = 1; i <= extractor.getExpressions().size(); i++) {
-            if (isFieldSearchCorrect(i, xIndex, yIndex)) {
-                validWords++;
+        int fieldToCheckX = xIndex - 1;
+        int fieldToCheckY = yIndex + 1;
+
+        if (isWithinBounds(fieldToCheckX, fieldToCheckY) && isWithinBounds(fieldToCheckX + 2, fieldToCheckY - 2)) {
+            if (isFieldSearchCorrect(1, fieldToCheckX, fieldToCheckY)) {
+                return 1;
+
             }
         }
-        return validWords;
+        return 0;
     }
 
     private boolean isFieldSearchCorrect(int iteratorForCharIndex, Integer xIndex, Integer yIndex) {
-        if (iteratorForCharIndex == charsToLookFor.length) {
-            return true;
+
+        if (isCharToLookFor(iteratorForCharIndex, xIndex, yIndex)) {
+            return startFieldSearch(iteratorForCharIndex, xIndex, yIndex, coordinatesToCheck.get(1), coordinatesToCheck.get(2), coordinatesToCheck.get(3));
         }
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                if (x == 0 && y == 0) {
-                    continue;
-                }
+        if (!isCharToLookFor(iteratorForCharIndex, xIndex, yIndex)) {
 
-                int fieldToCheckX = xIndex + x;
-                int fieldToCheckY = yIndex + y;
+            int nextFieldToCheckX = xIndex + 2;
+            int nextFieldToCheckY = yIndex - 2;
 
-                if (fieldToCheckX < firstX || fieldToCheckX > lastX || fieldToCheckY < firstY || fieldToCheckY > lastY) {
-                    continue;
-                }
-                if (isCharToLookFor(iteratorForCharIndex, fieldToCheckX, fieldToCheckY)) {
-                    if (isFieldSearchCorrect(iteratorForCharIndex + 1, fieldToCheckX, fieldToCheckY)) {
-                        return true;
-                    }
-                }
-
+            if (isCharToLookFor(iteratorForCharIndex, nextFieldToCheckX, nextFieldToCheckY)) {
+                return startFieldSearch(iteratorForCharIndex, nextFieldToCheckX, nextFieldToCheckY, coordinatesToCheck.get(4), coordinatesToCheck.get(5), coordinatesToCheck.get(6));
             }
         }
         return false;
     }
 
+    private boolean startFieldSearch(int iteratorForCharIndex, Integer xIndex, Integer yIndex, Integer[] firstCoordinates, Integer[] secondCoordinates, Integer[] thirdCoordinates) {
+        int fieldToCheckX = xIndex + firstCoordinates[0];
+        int fieldToCheckY = yIndex + firstCoordinates[1];
+
+        if (isCharToLookFor(iteratorForCharIndex, fieldToCheckX, fieldToCheckY)) {
+
+            return isCharToLookFor(iteratorForCharIndex + 2, xIndex + secondCoordinates[0], yIndex + secondCoordinates[1])
+                    && isCharToLookFor(iteratorForCharIndex + 2, xIndex + thirdCoordinates[0], yIndex + thirdCoordinates[1]);
+        }
+
+        fieldToCheckX = xIndex + secondCoordinates[0];
+        fieldToCheckY = yIndex + secondCoordinates[1];
+
+        if (isCharToLookFor(iteratorForCharIndex, fieldToCheckX, fieldToCheckY)) {
+
+            return isCharToLookFor(iteratorForCharIndex + 2, xIndex + firstCoordinates[0], yIndex + firstCoordinates[1])
+                    && isCharToLookFor(iteratorForCharIndex + 2, xIndex + thirdCoordinates[0], yIndex + thirdCoordinates[1]);
+        }
+        return false;
+    }
+
+    private boolean isWithinBounds(int xIndex, int yIndex) {
+        return xIndex >= firstX && xIndex <= lastX && yIndex >= firstY && yIndex <= lastY;
+    }
+
     private boolean isCharToLookFor(int iteratorForCharIndex, int fieldToCheckX, int fieldToCheckY) {
         for (Integer[] indexArray : extractor.getExpressions().get(iteratorForCharIndex)) {
+            if (indexArray[0] > fieldToCheckY) {
+                return false;
+            }
             if (fieldToCheckY == indexArray[0] && fieldToCheckX == indexArray[1]) {
                 return true;
             }
@@ -87,9 +124,12 @@ public class ExtractorAnalyzer {
         return map;
     }
 
+    public Collection<Integer> getValues() {
+        return values;
+    }
+
 
     public static void main(String[] args) throws IOException {
         ExtractorAnalyzer analyzer = new ExtractorAnalyzer();
-        System.out.println();
     }
 }
