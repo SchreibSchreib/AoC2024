ed608bc745480e8001d7a4b4f74a86c4d58301eb
Felix Schreiber <felixschreiber89@gmail.com>
2025-02-09 11:39:09 +0100

    Finished PrizeFilter to filter between winnable and unwinnable Expressions...now needs calculation

--- src/Days/Day13/PartOne/PrizeFilter.java
+++ src/Days/Day13/PartOne/PrizeFilter.java
@@ -4,14 +4,16 @@ import Days.Day13.InputFormatter;
 import Interfaces.InputManipulatable;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PrizeFilter {
 
     private final InputManipulatable<Map<Integer, List<Integer[]>>> inputManipulatable;
-    private Map<Integer, Integer[]> winnablePrizes;
-    private Map<Integer, Integer[]> unWinnablePrizes;
+    private final List<List<Integer[]>> winnablePrizes = new ArrayList<>();
+    private final List<List<Integer[]>> unWinnablePrizes = new ArrayList<>();
 
     public PrizeFilter() throws IOException {
         inputManipulatable = new InputFormatter();
@@ -19,8 +21,28 @@ public class PrizeFilter {
     }
 
     private void loadMaps() {
-        for (Map.Entry<Integer,List<Integer[]>> entry : inputManipulatable.getConvertedInput().entrySet()) {
-            for
+        for (Map.Entry<Integer, List<Integer[]>> entry : inputManipulatable.getConvertedInput().entrySet()) {
+            int[] xValues = new int[]{entry.getValue().get(0)[0], entry.getValue().get(1)[0]};
+            int[] yValues = new int[]{entry.getValue().get(0)[1], entry.getValue().get(1)[1]};
+            int[] targetValues = new int[]{entry.getValue().get(2)[0], entry.getValue().get(2)[1]};
+
+            if (isGreatestCommonDivisorDividableToTarget(xValues, targetValues[0]) && isGreatestCommonDivisorDividableToTarget(yValues, targetValues[1])) {
+                winnablePrizes.add(entry.getValue());
+            } else {
+                unWinnablePrizes.add(entry.getValue());
+            }
+
         }
+    }
+
+    private boolean isGreatestCommonDivisorDividableToTarget(int[] values, int targetValue) {
+        int biggerValue = Math.max(values[0], values[1]);
+        int smallerValue = Math.min(values[0], values[1]);
+        return targetValue % new GreatestCommonDivisorCalculator(biggerValue, smallerValue).getGreatestCommonDivisor() == 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        PrizeFilter filter = new PrizeFilter();
+
     }
 }
